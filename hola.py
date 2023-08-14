4eb90c97e2b9a9d4626940cc0eae7c1019cba984
Alberto Martinez <umoqnier@gmail.com>
2023-08-14 17:19:58 -0600

    Agregando decoracion a mensaje
    Se agrega una funcion que decora un mensaje de hola mundo con el
    caracter =

--- hola.py
+++ hola.py
@@ -1,2 +1,6 @@
-print("Hola mundo cruel :)")
-print("Estoy muy cansado, me wa dormir")
+def decora_mensaje(mensaje: str) -> None:
+    """Esta funcion agrega una dercoracion
+    """
+    print("="*10, mensaje, "="*10)
+
+decora_mensaje("Hola mundo cruel :)")
